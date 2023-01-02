56cf796b9509404de6c56a12c0d3cad39b2ffc4d
Abhishek Raj <raj.abhishek0025@gmail.com>
2023-01-02 16:11:15 +0530

    added options and title props in radioGroup

--- src/components/RadioGroup/index.tsx
+++ src/components/RadioGroup/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import { default as MUIRadioGroup, RadioGroupProps as MUIRadioGroupProps } from '@mui/material/RadioGroup';
 
-export interface RadioGroupProps extends MUIRadioGroupProps {
+import FormLabel from '@mui/material/FormLabel';
+
+import { Radio } from '../Radio/index';
+
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+interface FormLabelParams {
     value: string;
+    displayText: string;
 }
 
-export const RadioGroup: React.FC<RadioGroupProps> = ({ children, ...rest }) => {
-    return <MUIRadioGroup {...rest}>{children && children}</MUIRadioGroup>;
+export interface RadioGroupProps extends MUIRadioGroupProps {
+    title: string;
+    options: FormLabelParams[];
+    name: string;
+}
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({ options, title, ...rest }) => {
+    return (
+        <>
+            <FormLabel>{title}</FormLabel>
+            <MUIRadioGroup {...rest}>
+                {options.map((item, index) => {
+                    return <FormControlLabel key={index} value={item.value} control={<Radio />} label={item.displayText} />;
+                })}
+            </MUIRadioGroup>
+        </>
+    );
 };

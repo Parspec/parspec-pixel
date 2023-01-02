1e664dfe25ce7f3fee6372f20e64c0965b921605
Abhishek Raj <raj.abhishek0025@gmail.com>
2023-01-02 09:11:27 +0530

    removed the themeProvider

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { default as MUITextField, TextFieldProps as MUITextFieldProps } from '@mui/material/TextField';
 
-import { ThemeProvider } from '../../theme/ThemeProvider';
-
 export interface TextFieldProps extends Omit<MUITextFieldProps, 'margin' | 'classes' | 'sx'> {
     variant: 'standard' | 'outlined' | 'filled';
     color: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning';
     error: boolean;
 }
 
-export const TextField: React.FC<TextFieldProps> = (props) => {
-    return (
-        <ThemeProvider>
-            <MUITextField {...props} />
-        </ThemeProvider>
-    );
-};
+export const TextField: React.FC<TextFieldProps> = (props) => <MUITextField {...props} />;
 
 TextField.defaultProps = {
     variant: 'outlined',

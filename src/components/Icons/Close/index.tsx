e6739c009ddde2abdb8bcea90247b60f072f468b
sraturi <sraturi@xemplarinsights.com>
2023-01-03 15:07:50 +0530

    uninstalled external styled component package, added 2 more icons

---
+++ src/components/Icons/Close/index.tsx
@@ -0,0 +1,5 @@
+import {default as MUICloseIcon} from '@mui/icons-material/Close';
+import {IconsType} from '../';
+
+export const CloseIcon: React.FC<IconsType> = (props) => <MUICloseIcon fontSize={props?.fontSize} color='inherit' />
+  
---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,13 @@
+import { CloseIcon } from "./Close";
+import { SearchIcon } from "./Search";
+import { UnfoldMoreIcon } from "./UnfoldMore";
+
+export interface IconsType{
+  fontSize?: "large" | "medium" | "small" | undefined;
+}
+
+export const Icons = {
+  CloseIcon,
+  SearchIcon,
+  UnfoldMoreIcon
+}

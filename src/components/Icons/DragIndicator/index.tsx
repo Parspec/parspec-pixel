b499907a4432aab4aff9b03c5b1e567c5651cd4e
sraturi <sraturi@xemplarinsights.com>
2023-01-05 10:59:59 +0530

    removing icons pull

--- src/components/Icons/DragIndicator/index.tsx
+++
@@ -1,4 +0,0 @@
-import {default as MUIDragIndicatorIcon} from '@mui/icons-material/DragIndicator';
-import {IconsType} from '../';
-
-export const DragIndicatorIcon: React.FC<IconsType> = (props) => <MUIDragIndicatorIcon fontSize={props?.fontSize} color='inherit' />
--- src/components/Icons/Search/index.tsx
+++
@@ -1,4 +0,0 @@
-import {default as MUISearchIcon} from '@mui/icons-material/Search';
-import {IconsType} from '../';
-
-export const SearchIcon: React.FC<IconsType> = (props) => <MUISearchIcon fontSize={props?.fontSize} color='inherit' />

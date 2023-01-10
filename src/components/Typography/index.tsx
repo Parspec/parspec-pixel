6790cce07890fb4dfd4ce83c9661798eae5eaed0
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-01-10 15:47:22 +0530

    updated Typography props

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,6 +1,6 @@
 import { default as MUITypography, TypographyProps as MUITypographyProps } from '@mui/material/Typography';
 
-interface TypographyProps extends Pick<MUITypographyProps, 'color' | 'fontWeight' | 'textTransform' | 'variant' | 'fontSize' | 'letterSpacing' | 'lineHeight' | 'children'> {
+interface TypographyProps extends Omit<MUITypographyProps, 'color'> {
     color?: 'primary' | 'secondary';
 }
 

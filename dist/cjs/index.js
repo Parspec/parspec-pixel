3b4a5a8c1d206240989c930d10fa344cf41a3f20
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-01-11 13:39:40 +0530

    added build for styled

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -12,4 +12,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./components"), exports);
 __exportStar(require("./theme"), exports);
+__exportStar(require("./styles"), exports);
 //# sourceMappingURL=index.js.map

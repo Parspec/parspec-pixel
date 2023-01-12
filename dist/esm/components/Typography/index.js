606a1f344fcaac1d710e3b8562042c8ef482e6df
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-01-12 15:21:30 +0530

    added changes in Typography component

--- dist/esm/components/Typography/index.js
+++ dist/esm/components/Typography/index.js
@@ -10,26 +10,14 @@ const BodyText = (props) => {
 BodyText.defaultProps = {
     color: 'secondary'
 };
-const H1 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h1" }));
-const H2 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h2" }));
-const H3 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h3" }));
-const H4 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h4" }));
-const H5 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h5" }));
-const H6 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h6" }));
-const BodyBig = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '18px', letterSpacing: '0.15px', lineHeight: '28px' }));
-const BodyMedium = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '16px', letterSpacing: '0.44px', lineHeight: '24px' }));
-const BodySmall = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '14px', letterSpacing: '0.25px', lineHeight: '16px' }));
-const BodyXS = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '12px', lineHeight: '15px' }));
-export const Typography = {
-    H1,
-    H2,
-    H3,
-    H4,
-    H5,
-    H6,
-    BodyBig,
-    BodyMedium,
-    BodySmall,
-    BodyXS
-};
+export const H1 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h1" }));
+export const H2 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h2" }));
+export const H3 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h3" }));
+export const H4 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h4" }));
+export const H5 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h5" }));
+export const H6 = (props) => _jsx(Heading, Object.assign({}, props, { variant: "h6" }));
+export const BodyBig = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '18px', letterSpacing: '0.15px', lineHeight: '28px' }));
+export const BodyMedium = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '16px', letterSpacing: '0.44px', lineHeight: '24px' }));
+export const BodySmall = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '14px', letterSpacing: '0.25px', lineHeight: '16px' }));
+export const BodyXS = (props) => _jsx(BodyText, Object.assign({}, props, { fontSize: '12px', lineHeight: '15px' }));
 //# sourceMappingURL=index.js.map

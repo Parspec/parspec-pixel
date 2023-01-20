c61bdc66eeea808ca92f1968aff837b6065b9197
sraturi <sraturi@xemplarinsights.com>
2023-01-20 11:39:35 +0530

    changesBasedonStandUp

--- src/components/Modal/Footer.tsx
+++ src/components/Modal/Footer.tsx
@@ -12,10 +12,10 @@ export interface FooterProps {
 export const Footer: React.FC<FooterProps> = ({ onAccept, onReject, cancelButtonLabel, continueButtonLabel, isLoading }) => {
     return (
         <Box display="flex" justifyContent="end" gap={1}>
-            <Button color="secondary" variant="outlined" onClick={onReject} disabled={isLoading} isLoading={isLoading}>
+            <Button color="secondary" variant="outlined" onClick={onReject}>
                 {cancelButtonLabel}
             </Button>
-            <Button color="primary" variant="contained" onClick={onAccept} disabled={isLoading} isLoading={isLoading}>
+            <Button color="primary" variant="contained" onClick={onAccept} isLoading={isLoading}>
                 {continueButtonLabel}
             </Button>
         </Box>

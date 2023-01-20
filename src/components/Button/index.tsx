401419164f3cae4bd0d669e580ecd5657a705151
sraturi <sraturi@xemplarinsights.com>
2023-01-20 13:06:50 +0530

    isLoading default false

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -20,5 +20,6 @@ export const Button: React.FunctionComponent<ButtonProps> = ({ disabled, isLoadi
 
 Button.defaultProps = {
     color: 'primary',
-    variant: 'contained'
+    variant: 'contained',
+    isLoading: false
 };

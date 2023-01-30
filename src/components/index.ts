835eb6deec349da4cdfd9c9859ab11b3add931d2
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-01-30 13:51:48 +0530

    ..

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,12 +5,8 @@ export * from './Modal';
 export * from './Typography';
 export * from './TextField';
 export * from './Icons';
-<<<<<<< HEAD
 export * from './AutoComplete';
 export * from './Checkbox';
 export * from './Chip';
 export * from './Select';
 export * from './Grid';
-=======
-export * from './RadioGroup';
->>>>>>> 12c7931 (..)

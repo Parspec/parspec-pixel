712a922d90088663c4181203e38b1ec4e1abf3f0
sraturi <sraturi@xemplarinsights.com>
2023-02-09 10:04:20 +0530

    final

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,10 +1,7 @@
 import { default as MUITooltip, TooltipProps as MUITooltipProps } from '@mui/material/Tooltip';
 import { Box } from '../Box';
 
-interface TooltipProps extends MUITooltipProps {
-    title: string;
-}
-export const Tooltip: React.FC<TooltipProps> = ({ children, title, placement, ...rest }) => {
+export const Tooltip: React.FC<MUITooltipProps> = ({ children, title, placement, ...rest }) => {
     return (
         <MUITooltip title={title} placement={placement} {...rest}>
             <Box width={'max-content'}>{children}</Box>

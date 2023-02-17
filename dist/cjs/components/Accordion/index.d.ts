50268c333a897981233fd3249d6f68bed04f15d2
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-02-17 16:36:19 +0530

    removed dist folder

--- dist/cjs/components/Accordion/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { AccordionProps as MUIAccordionProps } from '@mui/material/Accordion';
-export interface AccordionProps extends Omit<MUIAccordionProps, 'classes' | 'children'> {
-    summary: React.ReactNode;
-    details: React.ReactNode;
-    variant: 'outlined' | 'elevation';
-}
-export declare const Accordion: React.FC<AccordionProps>;
--- dist/cjs/components/Modal/ModalHeader.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface ModalHeaderHeaderProps {
-    title: string;
-    onClose: () => void;
-    children?: React.ReactNode;
-}
-export declare const ModalHeader: React.FC<ModalHeaderHeaderProps>;

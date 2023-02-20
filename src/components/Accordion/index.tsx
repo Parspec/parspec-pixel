49fd6c88820b6efa2a4e12468ea607f09d63d396
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-02-20 12:26:10 +0530

    added Accordion and Switch

---
+++ src/components/Accordion/index.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react';
+
+import { default as MUIAccordion, AccordionProps as MUIAccordionProps } from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export interface AccordionProps extends Omit<MUIAccordionProps, 'classes' | 'children'> {
+    summary: React.ReactNode;
+    details: React.ReactNode;
+    variant?: 'outlined' | 'elevation';
+}
+
+export const Accordion: React.FC<AccordionProps> = forwardRef<HTMLDivElement, AccordionProps>(({ summary, details, variant, ...rest }, ref) => (
+    <MUIAccordion ref={ref} TransitionProps={{ unmountOnExit: true }} {...rest}>
+        <AccordionSummary
+            sx={{
+                flexDirection: 'row-reverse'
+            }}
+            expandIcon={<ExpandMoreIcon />}
+        >
+            {summary}
+        </AccordionSummary>
+        <AccordionDetails>{details}</AccordionDetails>
+    </MUIAccordion>
+));
+
+Accordion.defaultProps = {
+    variant: 'outlined'
+};

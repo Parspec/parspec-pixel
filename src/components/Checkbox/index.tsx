fcdad008578a1c594535380250d281e0a8ccf7e6
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-02-22 12:10:02 +0530

    removed color and size from destructuring props

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -7,11 +7,7 @@ export interface CheckboxProps extends Omit<MUICheckboxProps, 'classes' | 'sx'> 
     color?: 'primary' | 'secondary' | 'tertiary';
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ label, size, color, ...rest }) => (
-    <>
-        <FormControlLabel control={<MUICheckbox {...rest} size={size} color={color} />} label={label} />
-    </>
-);
+export const Checkbox: React.FC<CheckboxProps> = ({ label, ...rest }) => <FormControlLabel control={<MUICheckbox {...rest} />} label={label} />;
 
 Checkbox.defaultProps = {
     size: 'medium',

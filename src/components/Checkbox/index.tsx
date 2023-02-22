4e768492e8630b34a7c70e71a3cb3fc46489daf3
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-02-22 11:30:52 +0530

    added color support

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -4,14 +4,16 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 export interface CheckboxProps extends Omit<MUICheckboxProps, 'classes' | 'sx'> {
     label: string;
     size?: 'small' | 'medium';
+    color?: 'primary' | 'secondary' | 'tertiary';
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ label, size, ...rest }) => (
+export const Checkbox: React.FC<CheckboxProps> = ({ label, size, color, ...rest }) => (
     <>
-        <FormControlLabel control={<MUICheckbox {...rest} size={size} />} label={label} />
+        <FormControlLabel control={<MUICheckbox {...rest} size={size} color={color} />} label={label} />
     </>
 );
 
 Checkbox.defaultProps = {
-    size: 'medium'
+    size: 'medium',
+    color: 'tertiary'
 };

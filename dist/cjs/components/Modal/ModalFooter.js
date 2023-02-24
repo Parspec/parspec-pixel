f806aa9073ac3fb2c30c7c603d95024af3df1714
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-02-24 17:30:03 +0530

    added modal updated

--- dist/cjs/components/Modal/ModalFooter.js
+++ dist/cjs/components/Modal/ModalFooter.js
@@ -4,8 +4,8 @@ exports.ModalFooter = void 0;
 const jsx_runtime_1 = require("react/jsx-runtime");
 const Box_1 = require("../Box");
 const Button_1 = require("../Button");
-const ModalFooter = ({ onAccept, onReject, cancelButtonLabel, continueButtonLabel, isLoading }) => {
-    return ((0, jsx_runtime_1.jsxs)(Box_1.Box, Object.assign({ display: "flex", justifyContent: "end", gap: 2 }, { children: [(0, jsx_runtime_1.jsx)(Button_1.Button, Object.assign({ color: "secondary", variant: "outlined", onClick: onReject }, { children: cancelButtonLabel })), (0, jsx_runtime_1.jsx)(Button_1.Button, Object.assign({ color: "primary", variant: "contained", onClick: onAccept, isLoading: isLoading }, { children: continueButtonLabel }))] })));
+const ModalFooter = ({ onAccept, onReject, cancelButtonLabel, continueButtonLabel, isLoading, helperText }) => {
+    return ((0, jsx_runtime_1.jsxs)(Box_1.Box, Object.assign({ display: "flex", justifyContent: "space-between", alignItems: 'center' }, { children: [(0, jsx_runtime_1.jsx)(Box_1.Box, { children: helperText }), (0, jsx_runtime_1.jsxs)(Box_1.Box, Object.assign({ display: 'flex', gap: 2 }, { children: [(0, jsx_runtime_1.jsx)(Button_1.Button, Object.assign({ color: "secondary", variant: "outlined", onClick: onReject }, { children: cancelButtonLabel })), (0, jsx_runtime_1.jsx)(Button_1.Button, Object.assign({ color: "primary", variant: "contained", onClick: onAccept, isLoading: isLoading }, { children: continueButtonLabel }))] }))] })));
 };
 exports.ModalFooter = ModalFooter;
 exports.ModalFooter.defaultProps = {
@@ -13,6 +13,7 @@ exports.ModalFooter.defaultProps = {
     continueButtonLabel: 'Submit',
     onAccept: () => { },
     onReject: () => { },
-    isLoading: false
+    isLoading: false,
+    helperText: (0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, {})
 };
 //# sourceMappingURL=ModalFooter.js.map
--- dist/esm/components/Modal/ModalFooter.js
+++ dist/esm/components/Modal/ModalFooter.js
@@ -1,14 +1,15 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
 import { Box } from '../Box';
 import { Button } from '../Button';
-export const ModalFooter = ({ onAccept, onReject, cancelButtonLabel, continueButtonLabel, isLoading }) => {
-    return (_jsxs(Box, Object.assign({ display: "flex", justifyContent: "end", gap: 2 }, { children: [_jsx(Button, Object.assign({ color: "secondary", variant: "outlined", onClick: onReject }, { children: cancelButtonLabel })), _jsx(Button, Object.assign({ color: "primary", variant: "contained", onClick: onAccept, isLoading: isLoading }, { children: continueButtonLabel }))] })));
+export const ModalFooter = ({ onAccept, onReject, cancelButtonLabel, continueButtonLabel, isLoading, helperText }) => {
+    return (_jsxs(Box, Object.assign({ display: "flex", justifyContent: "space-between", alignItems: 'center' }, { children: [_jsx(Box, { children: helperText }), _jsxs(Box, Object.assign({ display: 'flex', gap: 2 }, { children: [_jsx(Button, Object.assign({ color: "secondary", variant: "outlined", onClick: onReject }, { children: cancelButtonLabel })), _jsx(Button, Object.assign({ color: "primary", variant: "contained", onClick: onAccept, isLoading: isLoading }, { children: continueButtonLabel }))] }))] })));
 };
 ModalFooter.defaultProps = {
     cancelButtonLabel: 'Cancel',
     continueButtonLabel: 'Submit',
     onAccept: () => { },
     onReject: () => { },
-    isLoading: false
+    isLoading: false,
+    helperText: _jsx(_Fragment, {})
 };
 //# sourceMappingURL=ModalFooter.js.map

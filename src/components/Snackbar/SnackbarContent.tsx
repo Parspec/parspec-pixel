c0b81e3145d19819d206a38d7c183d5dd974fe2b
Anit Singh <anit.singh@algoscale.com>
2023-03-02 09:09:30 +0530

    Snackbar component

---
+++ src/components/Snackbar/SnackbarContent.tsx
@@ -0,0 +1,4 @@
+import { default as MUISnackbarContent, SnackbarContentProps as MUISnackbarContentProps } from '@mui/material/SnackbarContent';
+export const SnackbarContent: React.FC<MUISnackbarContentProps> = (props) => {
+    return <MUISnackbarContent {...props}></MUISnackbarContent>;
+};

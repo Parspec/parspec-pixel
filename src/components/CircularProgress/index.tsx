543ed9aedf604e4aad2719470fab554602e56c1f
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-03-06 15:21:33 +0530

    fixed circular progress

--- src/components/CircularProgress/index.tsx
+++ src/components/CircularProgress/index.tsx
@@ -1,12 +1,11 @@
 import { default as MUICircularProgress, CircularProgressProps as MUICircularProgressProps } from '@mui/material/CircularProgress';
 
-export interface CircularProgressProps extends Omit<MUICircularProgressProps, 'sx'> {
-    color?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | 'inherit';
-}
+export interface CircularProgressProps extends Omit<MUICircularProgressProps, 'sx'> {}
 
-export const CircularProgress: React.FC<CircularProgressProps> = ({ color, size, ...rest }) => <MUICircularProgress color={color} size={size} {...rest} />;
+export const CircularProgress: React.FC<CircularProgressProps> = (props) => <MUICircularProgress {...props} />;
 
 CircularProgress.defaultProps = {
-    color: 'inherit',
-    size: 20
+    color: 'primary',
+    size: 20,
+    thickness: 3.6
 };

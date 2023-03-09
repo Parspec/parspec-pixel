b9167f84f79cb161dafebd173ebc90fc25e6dda6
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-03-09 09:51:55 +0530

    date picker component

---
+++ src/components/DatePicker/index.tsx
@@ -0,0 +1,25 @@
+import { Dayjs } from 'dayjs';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export interface CustomDatePickerProps {
+    label?: string;
+    value?: Dayjs | null;
+    onChange?: (newValue: Dayjs | null) => void;
+    defaultValue?: Dayjs;
+}
+
+export const CustomDatePicker: React.FC<CustomDatePickerProps> = (props) => {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                sx={{
+                    width: '100%'
+                }}
+                {...props}
+            />
+        </LocalizationProvider>
+    );
+};

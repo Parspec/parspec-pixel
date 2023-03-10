16638a9e5131dfdfee306179aa5c40327cc65572
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-03-10 13:54:16 +0530

    inint commit

--- src/components/FileSelector/FileSelector.stories.tsx
+++ src/components/FileSelector/FileSelector.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { FileSelector } from './index';
+import { Box } from '@mui/system';
 
 export default {
     title: 'File Selector',
@@ -8,8 +9,25 @@ export default {
     argTypes: { onUpload: { action: 'onUpload' } }
 } as ComponentMeta<typeof FileSelector>;
 
-const Template: ComponentStory<typeof FileSelector> = (args) => (
-    <FileSelector url="https://hotfix-staging.parspec.xyz/api/generate_signed_url/" error="Wrong format" maxFiles={2} onUpload={(data) => console.log('uploaded', data)} />
+export const fileSelector: ComponentStory<typeof FileSelector> = (args) => (
+    <FileSelector {...args} url="https://hotfix-staging.parspec.xyz/api/generate_signed_url/" error="Wrong format" maxFiles={2} onUpload={(data) => console.log('uploaded', data)} />
 );
 
-export const Primary = Template.bind({});
+fileSelector.args = {
+    placeholder: 'Drag and drop files here, or:',
+    error: 'Wrong format',
+    maxFiles: 2,
+    onUpload: (data) => console.log('uploaded', data)
+};
+
+export const customFileSelector: ComponentStory<typeof FileSelector> = (args) => (
+    <Box width={'178px'} height="218px" border="1px solid grey" margin={2} bgcolor={'#f3f5fa'}>
+        <FileSelector {...args} url="https://hotfix-staging.parspec.xyz/api/generate_signed_url/" maxFiles={2} onUpload={(data) => console.log('uploaded', data)} />
+    </Box>
+);
+
+customFileSelector.args = {
+    placeholder: 'Drag and drop files here, or:',
+    maxFiles: 2,
+    onUpload: (data) => console.log('uploaded', data)
+};

d7d745153e73e7b7f8c8519926729f0216dce079
kumarlovish <104341483+kumarlovish@users.noreply.github.com>
2023-03-22 17:10:20 +0530

    helper text added

--- src/components/AutoComplete/Autocomplete.tsx
+++ src/components/AutoComplete/Autocomplete.tsx
@@ -2,6 +2,7 @@ import { forwardRef } from 'react';
 
 import { TextField } from '../TextField';
 import { default as MUIAutocomplete } from '@mui/material/Autocomplete';
+import { BodyXS } from '../Typography';
 
 export type OptionType = {
     [index: string]: string | number;
@@ -20,38 +21,44 @@ export interface AutocompleteProps {
     multiple?: boolean;
     value?: string | OptionType | (string | OptionType)[] | null;
     defaultValue?: string | OptionType | (string | OptionType)[] | null;
-    onBlur: (event: React.SyntheticEvent) => void;
+    onBlur?: (event: React.SyntheticEvent) => void;
+    helperText?: string;
 }
 
 export const Autocomplete: React.FC<AutocompleteProps> = forwardRef<HTMLDivElement, AutocompleteProps>(
-    ({ id, label, color, variant, onChange, optionlabelkeyname, freeSolo, size, onBlur, ...props }, ref) => {
+    ({ id, label, color, variant, onChange, optionlabelkeyname, freeSolo, size, onBlur, helperText, ...props }, ref) => {
         const handleOnChange = (event: any, newValue: string | OptionType | (string | OptionType)[] | null) => {
             onChange({ ...event, target: { ...event.target, value: newValue } });
         };
 
         const handleFocusOut = (event: any) => {
-            onBlur(event.target.value);
+            if (onBlur) {
+                onBlur(event.target.value);
+            }
             console.log(event);
         };
         return (
-            <MUIAutocomplete
-                size={size}
-                fullWidth
-                {...props}
-                ref={ref}
-                id={id}
-                onBlur={handleFocusOut}
-                onChange={handleOnChange}
-                getOptionLabel={(option: OptionType | string): string => {
-                    if (typeof option === 'object') {
-                        return `${option[optionlabelkeyname]}`;
-                    }
+            <>
+                <MUIAutocomplete
+                    size={size}
+                    fullWidth
+                    {...props}
+                    ref={ref}
+                    id={id}
+                    onBlur={handleFocusOut}
+                    onChange={handleOnChange}
+                    getOptionLabel={(option: OptionType | string): string => {
+                        if (typeof option === 'object') {
+                            return `${option[optionlabelkeyname]}`;
+                        }
 
-                    return option;
-                }}
-                freeSolo={freeSolo}
-                renderInput={(params) => <TextField {...params} variant={variant} color={color} label={label} />}
-            />
+                        return option;
+                    }}
+                    freeSolo={freeSolo}
+                    renderInput={(params) => <TextField {...params} variant={variant} color={color} label={label} />}
+                />
+                {helperText && <BodyXS>{helperText}</BodyXS>}
+            </>
         );
     }
 );
@@ -61,5 +68,6 @@ Autocomplete.defaultProps = {
     variant: 'outlined',
     freeSolo: false,
     size: 'small',
-    multiple: false
+    multiple: false,
+    helperText: ''
 };

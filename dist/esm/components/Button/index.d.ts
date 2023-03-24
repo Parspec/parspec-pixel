f925af89334c8bae320ac89311735c4072958d92
sraturi <sraturi@xemplarinsights.com>
2023-03-24 21:09:22 +0530

    r

--- dist/esm/components/Button/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { ButtonProps as MUIButtonProps } from '@mui/material/Button';
-export interface ButtonProps extends Omit<MUIButtonProps, 'classes'> {
-    color?: 'primary' | 'secondary' | 'tertiary' | 'error';
-    isLoading?: boolean;
-}
-export declare const Button: import("react").ForwardRefExoticComponent<Omit<ButtonProps, "ref"> & import("react").RefAttributes<HTMLButtonElement>>;

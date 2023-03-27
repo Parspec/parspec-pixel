71edc0f0df98550ce462ba53bf2c6af206b52c95
abhishekraj1007 <abhishek.raj@algoscale.com>
2023-03-27 09:16:16 +0530

    added size property to datepicker

--- dist/esm/components/DatePicker/index.d.ts
+++ dist/esm/components/DatePicker/index.d.ts
@@ -1,4 +1,9 @@
 /// <reference types="react" />
 import { Dayjs } from 'dayjs';
-import { DesktopDatePickerProps } from '@mui/x-date-pickers/DesktopDatePicker';
-export declare const CustomDatePicker: React.FC<DesktopDatePickerProps<Dayjs>>;
+import { DatePickerProps } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+export { Dayjs, dayjs };
+export interface CustomDatePickerProps<Dayjs> extends DatePickerProps<Dayjs> {
+    size?: 'small' | 'medium';
+}
+export declare const CustomDatePicker: React.FC<CustomDatePickerProps<Dayjs>>;

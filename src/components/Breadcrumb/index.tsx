c5dcc7a097270747d492bca8c1123f778b0f2103
Charul-Parspec <charul@parspec.io>
2023-04-03 12:58:54 +0530

    update as per review comments

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,5 +1,5 @@
 import { default as MUIBreadcrumb, BreadcrumbsProps as MUIBreadcrumbsProps } from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
+import MUILink, { LinkProps as MUILinkProps } from '@mui/material/Link';
 import { BodyXS } from '../Typography';
 
 interface OptionTypes {
@@ -9,7 +9,15 @@ interface OptionTypes {
 
 export interface BreadcrumbsProps extends Omit<MUIBreadcrumbsProps, 'classes' | 'sx'> {
     options: OptionTypes[];
-    component: React.ElementType;
+    component?: React.ElementType;
+}
+
+function Link<C extends React.ElementType>({ children, component, ...restProps }: MUILinkProps<C, { component?: C }>) {
+    return (
+        <MUILink component={component} {...restProps}>
+            {children}
+        </MUILink>
+    );
 }
 
 export const Breadcrumb: React.FC<BreadcrumbsProps> = ({ options, component, ...rest }) => {

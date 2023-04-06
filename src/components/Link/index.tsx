31b86abe7cd258a10f875d59bb88b25db3a0560d
Charul-Parspec <charul@parspec.io>
2023-04-06 10:52:39 +0530

    add link component

---
+++ src/components/Link/index.tsx
@@ -0,0 +1,9 @@
+import { Link as MUILink, LinkProps as MUILinkProps } from '@mui/material';
+
+export default function Link<C extends React.ElementType>({ children, component, ...restProps }: MUILinkProps<C, { component?: C }>) {
+    return (
+        <MUILink component={component} {...restProps}>
+            {children}
+        </MUILink>
+    );
+}

82f62821142d769ebe384a7e452b34ef7afa0f50
hrishi-Parspec <127725552+hrishi-Parspec@users.noreply.github.com>
2023-04-06 13:52:31 +0530

    add: XS sizing to button and xs string to szie prop in MUI Button.d.ts file

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,25 @@
 import { forwardRef } from 'react';
 import { default as MUIButton, ButtonProps as MUIButtonProps } from '@mui/material/Button';
 import { CircularProgress } from '../CircularProgress';
+import { ButtonSizeType } from '../../Shared/interfaces';
 
 export interface ButtonProps extends Omit<MUIButtonProps, 'classes'> {
     color?: 'primary' | 'secondary' | 'tertiary' | 'error' | 'success';
     isLoading?: boolean;
+    size?: ButtonSizeType;
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ disabled, isLoading, color, ...rest }, ref) => {
+const XS_CLASS = {
+    height: '24px'
+};
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ disabled, isLoading, color, size = 'small', ...rest }, ref) => {
     return (
         <MUIButton
             ref={ref}
             {...rest}
             color={color}
-            sx={disabled || isLoading ? { opacity: 0.5, pointerEvents: 'none' } : {}}
+            sx={disabled || isLoading ? { opacity: 0.5, pointerEvents: 'none', ...(size === 'xs' ? XS_CLASS : {}) } : { ...(size === 'xs' ? XS_CLASS : {}) }}
             startIcon={isLoading ? <CircularProgress color={'inherit'} size="sm" /> : null}
         />
     );

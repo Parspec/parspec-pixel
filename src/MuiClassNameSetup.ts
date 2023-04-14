ea7061d9e3b1a5e493dffc64f7c0ae0e0962982c
Abhishek Raj <abhishek.raj@algoscale.com>
2023-04-14 19:12:57 +0530

    Class generator file removed

--- src/MuiClassNameSetup.ts
+++
@@ -1,3 +0,0 @@
-import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
-
-ClassNameGenerator.configure((componentName) => `pixel-${componentName.replace('Mui', 'Pixel')}`);

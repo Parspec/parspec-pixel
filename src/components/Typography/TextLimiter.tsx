68fb0eb238697cc5f76a7ec69067040b39fe7c41
Charul-Parspec <charul@parspec.io>
2023-04-25 19:04:31 +0530

    fix md-5659

--- src/components/Typography/TextLimiter.tsx
+++ src/components/Typography/TextLimiter.tsx
@@ -13,7 +13,8 @@ interface TextLimiterProps {
 
 const StyledToolTip = styled(({ className, ...props }: TooltipProps) => <Tooltip {...props} classes={{ popper: className }} />)({
     [`& .${tooltipClasses.tooltip}`]: {
-        maxWidth: '60%'
+        maxWidth: '60%',
+        wordBreak: 'break-word'
     }
 });
 

ce5da4cdbde7a232f180cd22923016e151b89788
Atul midha <atulmidha@Atuls-MacBook-Pro.local>
2023-05-01 17:00:18 +0530

    fixed font size

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -28,12 +28,12 @@ BodyText.defaultProps = {
     lines: 1
 };
 
-export const H1: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h1" />;
-export const H2: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h2" />;
-export const H3: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h3" />;
-export const H4: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h4" />;
-export const H5: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h5" />;
-export const H6: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h6" />;
+export const H1: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h1" fontSize={'30px'} />;
+export const H2: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h2" fontSize={'28px'} />;
+export const H3: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h3" fontSize={'26px'} />;
+export const H4: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h4" fontSize={'24px'} />;
+export const H5: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h5" fontSize={'22px'} />;
+export const H6: React.FC<HeadingTypographyProps> = (props) => <Heading {...props} variant="h6" fontSize={'20px'} />;
 
 export const BodyBig: React.FC<BodyTypographyProps> = (props) => <BodyText {...props} fontSize={'18px'} letterSpacing={'0.15px'} lineHeight={'28px'} />;
 export const BodyMedium: React.FC<BodyTypographyProps> = (props) => <BodyText {...props} fontSize={'16px'} letterSpacing={'0.44px'} lineHeight={'24px'} />;

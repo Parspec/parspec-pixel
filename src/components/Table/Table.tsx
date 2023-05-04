e1e1ee775b7567fa4505ac845480fb872fd6ffc4
SudhanshuRaturi <sudhanshu.raturi@algoscale.com>
2023-05-04 16:29:45 +0530

    t

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -364,7 +364,9 @@ export const Table: React.FC<TableProps> = forwardRef((props, ref) => {
                         )}
                         {toolBarOptions?.includes('selectedItems') && selected > 0 && (
                             <Box p={1} pl={3} pr={2} bgcolor={'primary.main'} color={'secondary.contrastText'} display="flex" alignItems="center" gap={2}>
-                                <BodySmall color="secondary.contrastText">{selected} item(s) selected</BodySmall>
+                                <BodySmall color="secondary.contrastText" limit={false}>
+                                    {selected} item(s) selected
+                                </BodySmall>
                                 <IconButton onClick={closeBanner} sx={{ color: 'secondary.contrastText', margin: 0, padding: 0 }}>
                                     <CloseIcon fontSize="small" />
                                 </IconButton>

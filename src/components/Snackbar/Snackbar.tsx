4ce59ef8c046ca834d83ffb111fd0dcfa926a98f
Charul-Parspec <charul@parspec.io>
2023-05-09 11:20:09 +0530

    fix snackbar width

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -13,7 +13,7 @@ export const Snackbar: React.FC<MUISnackbarProps> = ({ ContentProps, ...props })
                     padding: '4px 16px',
                     maxWidth: '40vw',
                     '@media (min-width: 600px)': {
-                        minWidth: 160
+                        minWidth: '2ch'
                     }
                 }
             }}

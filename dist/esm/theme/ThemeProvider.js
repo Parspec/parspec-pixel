c8084e4dc229b3ffd7fc0977843671dd1d30c160
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-05-23 14:05:29 +0530

    changed text secondary color

--- dist/esm/theme/ThemeProvider.js
+++ dist/esm/theme/ThemeProvider.js
@@ -41,7 +41,7 @@ export const theme = createTheme({
         action: {},
         text: {
             primary: '#091535',
-            secondary: '#6B7280;'
+            secondary: '#D4D4D8'
         },
         tertiary: {
             light: '#ffe4bd',

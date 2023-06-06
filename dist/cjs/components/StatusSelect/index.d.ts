7f6792a1959ef9abb30fc375e42c57c70827b65a
Charul-Parspec <charul@parspec.io>
2023-06-06 18:47:35 +0530

    add build

--- dist/cjs/components/StatusSelect/index.d.ts
+++ dist/cjs/components/StatusSelect/index.d.ts
@@ -4,12 +4,12 @@ interface SelectMenuOption {
     [index: string]: string | number;
 }
 type ColorType = 'primary' | 'secondary' | 'tertiary' | 'info' | 'warning' | 'success' | 'error';
-export interface SelectProps extends Omit<MUISelectProps, 'classes' | 'label' | 'labelId' | 'input' | 'sx'> {
+export interface StatusSelectProps extends Omit<MUISelectProps, 'classes' | 'label' | 'labelId' | 'input' | 'sx'> {
     options: SelectMenuOption[];
     id?: string;
     optionLabelKeyname?: string;
     optionValueKeyname?: string;
     type?: ColorType;
 }
-export { SelectChangeEvent } from '@mui/material';
-export declare const StatusSelect: import("react").ForwardRefExoticComponent<Omit<SelectProps, "ref"> & import("react").RefAttributes<HTMLDivElement>>;
+export declare const StatusSelect: import("react").ForwardRefExoticComponent<Omit<StatusSelectProps, "ref"> & import("react").RefAttributes<HTMLDivElement>>;
+export {};

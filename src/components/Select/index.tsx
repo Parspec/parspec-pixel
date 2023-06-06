390dd0fded0cceca505b6b99caa160f22ded6279
Charul-Parspec <charul@parspec.io>
2023-06-06 18:25:06 +0530

    add status select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -10,9 +10,7 @@ interface SelectMenuOption {
     [index: string]: string | number;
 }
 
-type ColorType = 'primary' | 'secondary' | 'tertiary' | 'info' | 'warning' | 'success' | 'error';
-type SelectType = 'standard' | 'filled';
-
+type BorderColorType = 'primary' | 'secondary' | 'tertiary' | 'info' | 'warning' | 'success' | 'error';
 export interface SelectProps extends Omit<MUISelectProps, 'classes'> {
     label: string;
     options: SelectMenuOption[];
@@ -21,31 +19,29 @@ export interface SelectProps extends Omit<MUISelectProps, 'classes'> {
     size?: 'small' | 'medium';
     optionLabelKeyname?: string;
     optionValueKeyname?: string;
-    borderColor?: ColorType;
-    variant?: SelectType;
-    filledColorType?: ColorType;
+    borderColor?: BorderColorType;
 }
 
 export { SelectChangeEvent } from '@mui/material';
 
 interface FormControlProps extends MUIFormControlProps {
-    borderColor?: ColorType;
-    selectVariant: SelectType;
-    selectVariantColorType: ColorType;
+    borderColor?: BorderColorType;
 }
 
 const StyledFormControl = styled(FormControl, {
     shouldForwardProp(propName) {
         return !(propName === 'borderColor');
     }
-})<FormControlProps>(({ theme, borderColor, selectVariant, selectVariantColorType }) => {
-    const selectCss: Record<string, any> = {};
-    if (borderColor || selectVariant === 'filled') {
-        const colorValFromTheme = theme.palette?.[selectVariant === 'filled' ? selectVariantColorType : borderColor!]?.main;
-        selectCss['& label.Mui-focused, & label.MuiInputLabel-shrink'] = {
+})<FormControlProps>(({ theme, borderColor }) => {
+    if (!borderColor) {
+        return {};
+    }
+    const colorValFromTheme = theme.palette?.[borderColor]?.main;
+    return {
+        '& label.Mui-focused, & label.MuiInputLabel-shrink': {
             color: colorValFromTheme
-        };
-        selectCss['& .MuiOutlinedInput-root'] = {
+        },
+        '& .MuiOutlinedInput-root': {
             '& fieldset': {
                 borderColor: colorValFromTheme
             },
@@ -55,35 +51,22 @@ const StyledFormControl = styled(FormControl, {
             '&.Mui-focused fieldset': {
                 borderColor: colorValFromTheme
             }
-        };
-    }
-    if (selectVariant === 'filled') {
-        const colorType = selectVariantColorType;
-        const bgColorValFromTheme = theme.palette?.[colorType]?.light;
-        const colorValFromTheme = theme.palette?.[colorType]?.main;
-        const selectRootCss = { backgroundColor: bgColorValFromTheme, color: colorValFromTheme };
-        selectCss['& .MuiSelect-select'] = selectRootCss;
-        selectCss['& .MuiSelect-select:hover'] = selectRootCss;
-    }
-    return {
-        ...selectCss
+        }
     };
 });
 
-export const Select = forwardRef<HTMLDivElement, SelectProps>(
-    ({ id, labelId, options, size, label, optionLabelKeyname = 'label', optionValueKeyname = 'value', borderColor, variant = 'standard', filledColorType = 'primary', ...rest }, ref) => (
-        <StyledFormControl fullWidth ref={ref} size={size} borderColor={borderColor} selectVariant={variant} selectVariantColorType={filledColorType}>
-            <InputLabel id={labelId}>{label}</InputLabel>
-            <MUISelect {...rest} labelId={labelId} label={label} id={id}>
-                {options.map((item, index) => (
-                    <MenuItem key={index} value={item[optionValueKeyname]}>
-                        {item[optionLabelKeyname]}
-                    </MenuItem>
-                ))}
-            </MUISelect>
-        </StyledFormControl>
-    )
-);
+export const Select = forwardRef<HTMLDivElement, SelectProps>(({ id, labelId, options, size, label, optionLabelKeyname = 'label', optionValueKeyname = 'value', borderColor, ...rest }, ref) => (
+    <StyledFormControl fullWidth ref={ref} size={size} borderColor={borderColor}>
+        <InputLabel id={labelId}>{label}</InputLabel>
+        <MUISelect {...rest} labelId={labelId} label={label} id={id}>
+            {options.map((item, index) => (
+                <MenuItem key={index} value={item[optionValueKeyname]}>
+                    {item[optionLabelKeyname]}
+                </MenuItem>
+            ))}
+        </MUISelect>
+    </StyledFormControl>
+));
 
 Select.defaultProps = {
     label: 'Select',

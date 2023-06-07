d25acc8b6a4ed9f4f859ecc13f8112741bd18f5b
Abhishek Raj <abhishek.raj@algoscale.com>
2023-06-07 17:19:57 +0530

    removed custom styling

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -7,22 +7,7 @@ export interface CheckboxProps extends Omit<MUICheckboxProps, 'classes'> {
     color?: 'primary' | 'secondary' | 'tertiary';
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ label, ...rest }) => (
-    <FormControlLabel
-        control={
-            <MUICheckbox
-                sx={{
-                    padding: 0,
-                    backgroundColor: 'white',
-                    borderRadius: 0,
-                    '&:hover': { bgcolor: 'white' }
-                }}
-                {...rest}
-            />
-        }
-        label={label}
-    />
-);
+export const Checkbox: React.FC<CheckboxProps> = ({ label, ...rest }) => <FormControlLabel control={<MUICheckbox {...rest} />} label={label} />;
 
 Checkbox.defaultProps = {
     size: 'small'

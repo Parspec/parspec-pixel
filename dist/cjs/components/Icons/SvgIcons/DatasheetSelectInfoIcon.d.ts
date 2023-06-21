665685c56973b42b1af1a9966f2ce9958640ec70
SudhanshuRaturi <sudhanshu.raturi@algoscale.com>
2023-06-21 09:56:27 +0530

    build removed

--- dist/cjs/components/Icons/SvgIcons/DatasheetSelectInfoIcon.d.ts
+++
@@ -1,8 +0,0 @@
-import { SvgIconProps } from '../SvgIcon';
-export declare const DatasheetSelectInfoIcon: {
-    ({ fontSize, color }: SvgIconProps): import("react/jsx-runtime").JSX.Element;
-    defaultProps: {
-        fontSize: string;
-        color: string;
-    };
-};
--- dist/esm/components/ProgressBar/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { CircularProgressProps } from '@mui/material/CircularProgress';
-declare const ProgressBar: (props: CircularProgressProps & {
-    progress: number;
-}) => import("react/jsx-runtime").JSX.Element;
-export default ProgressBar;

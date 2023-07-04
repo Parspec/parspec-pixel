9011af676489393109bfc329ee8d856a1dbf0732
Gautam Kumar <gautam@parspec.io>
2023-07-04 10:25:37 +0530

    added check icon

--- dist/cjs/components/Img/index.d.ts
+++ dist/cjs/components/Img/index.d.ts
@@ -1,7 +1,6 @@
-/// <reference types="react" />
 export interface ImgProps {
     src: string;
     width: string;
     height: string;
 }
-export declare const Img: ({ src, width, height, ...rest }: ImgProps) => JSX.Element;
+export declare const Img: ({ src, width, height, ...rest }: ImgProps) => import("react/jsx-runtime").JSX.Element;

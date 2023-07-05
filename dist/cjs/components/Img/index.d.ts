68a087b5a31d4eaf66c2da79f22cb97ab7d34e4b
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-07-05 13:28:24 +0530

    added new build for new icons

--- dist/cjs/components/Img/index.d.ts
+++ dist/cjs/components/Img/index.d.ts
@@ -1,6 +1,7 @@
+/// <reference types="react" />
 export interface ImgProps {
     src: string;
     width: string;
     height: string;
 }
-export declare const Img: ({ src, width, height, ...rest }: ImgProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Img: ({ src, width, height, ...rest }: ImgProps) => JSX.Element;
--- dist/cjs/components/Info/index.d.ts
+++ dist/cjs/components/Info/index.d.ts
@@ -1,5 +1,6 @@
+/// <reference types="react" />
 import { BoxProps } from '@mui/material';
 interface InfoProps extends Omit<BoxProps, 'border' | 'borderColor' | 'padding'> {
 }
-export declare function Info({ children, ...otherProps }: InfoProps): import("react/jsx-runtime").JSX.Element;
+export declare function Info({ children, ...otherProps }: InfoProps): JSX.Element;
 export {};

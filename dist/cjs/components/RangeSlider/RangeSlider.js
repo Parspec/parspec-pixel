ee9028bab05504b28761d366879c075a47c15b26
abhijit-parspec <abhijit@Abhijits-MacBook-Pro.local>
2023-07-24 17:00:18 +0530

    pushed range slider change

--- dist/cjs/components/RangeSlider/RangeSlider.js
+++ dist/cjs/components/RangeSlider/RangeSlider.js
@@ -19,35 +19,55 @@ const NumberTextField = (0, styles_1.styled)(TextField_1.TextField)(({ theme }) 
     '& .MuiInputBase-input': {
         padding: theme.spacing(2)
     }
-    // '& .MuiInputAdornment-positionEnd': {
-    //     marginRight: 0
-    // }
 }));
-function getAdjustedValues(valueArr, minVal, maxVal) {
-    let [value1, value2] = valueArr;
-    if (value1 > value2 || value1 > maxVal) {
-        // console.log('1', [value1, value2]);
-        value1 = value2 - 1;
-    }
-    if (value1 < minVal) {
-        // console.log('2', [value1, value2]);
-        value1 = minVal;
-    }
-    if (value2 < value1 || value2 < minVal) {
-        // console.log('3', [value1, value2]);
-        value2 = value1 + 1;
-    }
-    if (value2 > maxVal) {
-        // console.log('4', [value1, value2]);
-        value2 = maxVal;
-    }
-    return [value1, value2];
-}
 exports.RangeSlider = (0, react_1.forwardRef)((props, ref) => {
     const { value, size, step, marks, min, max, color, headerTitle, disabled, rightTextfieldWidth, leftTextfieldWidth, textfieldHeight, onChange: onRangeChange, onRangeBlur = () => { }, onSliderMouseUp = () => { }, onTextfieldBlur = () => { }, onTextfieldEnterKeyDown = () => { }, showPlus, disableSwap } = props;
     const [textFieldVal, setTextFieldVal] = (0, react_1.useState)({ lowerField: value[0], upperField: value[1] });
+    const [activeThumbValue, setActiveThumbValue] = (0, react_1.useState)(textFieldVal.upperField);
+    const [plusToggle, setPlusToggle] = (0, react_1.useState)(true);
+    function getAdjustedValues(valueArr, minVal, maxVal) {
+        let [value1, value2] = valueArr;
+        if (value1 > value2 || value1 > maxVal) {
+            // console.log('1', [value1, value2]);
+            value1 = value2 - 1;
+        }
+        if (value1 < minVal) {
+            // console.log('2', [value1, value2]);
+            value1 = minVal;
+        }
+        if (value2 < value1 || value2 < minVal) {
+            // console.log('3', [value1, value2]);
+            value2 = value1 + 1;
+        }
+        if (value2 > maxVal) {
+            // console.log('4', [value1, value2]);
+            value2 = maxVal;
+        }
+        if (value1 === value2) {
+            if (activeThumbValue === 0) {
+                if (Math.min(value1, value2 - 1) < min) {
+                    value1 = min;
+                    value2 = value1 + 1;
+                }
+                else {
+                    value1 = Math.min(value1, value2 - 1);
+                }
+            }
+            else {
+                if (Math.max(value2, value1 + 1) > max) {
+                    value2 = max;
+                    value1 = value2 - 1;
+                }
+                else {
+                    value2 = Math.max(value2, value1 + 1);
+                }
+            }
+        }
+        return [value1, value2];
+    }
     (0, react_1.useEffect)(() => {
         setTextFieldVal(() => (Object.assign(Object.assign({}, textFieldVal), { lowerField: value[0], upperField: value[1] })));
+        setActiveThumbValue(textFieldVal.upperField);
     }, [value]);
     (0, react_1.useEffect)(() => {
         const adjustedValues = getAdjustedValues(value, min, max);
@@ -55,10 +75,25 @@ exports.RangeSlider = (0, react_1.forwardRef)((props, ref) => {
             onRangeChange(adjustedValues);
         }
     }, [value[0], value[1]]);
-    const sliderChangeHandler = (e) => {
-        const data = e.target.value;
-        const newData = [Number(data[0]), Number(data[1])];
-        setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
+    (0, react_1.useEffect)(() => {
+        if (!plusToggle) {
+            setPlusToggle(true);
+        }
+    }, [textFieldVal.upperField]);
+    const sliderChangeHandler = (e, newValue, activeThumb) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+        let newData;
+        if (activeThumb === 0) {
+            newData = [Math.min(newValue[0], textFieldVal.upperField - 1), textFieldVal.upperField];
+            setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
+        }
+        else {
+            newData = [textFieldVal.lowerField, Math.max(newValue[1], textFieldVal.lowerField + 1)];
+            setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
+        }
+        setActiveThumbValue(() => activeThumb);
         onRangeChange(newData);
     };
     const minChangeHandler = (event) => {
@@ -70,8 +105,14 @@ exports.RangeSlider = (0, react_1.forwardRef)((props, ref) => {
         }
     };
     const maxChangeHandler = (event) => {
-        const inputValue = event.target.value;
+        let inputValue = event.target.value;
+        if (inputValue.includes('+')) {
+            inputValue = inputValue.replace(/\+/g, '');
+        }
         const numericValue = Number(inputValue);
+        if (numericValue === max) {
+            return setPlusToggle(false);
+        }
         if (!isNaN(numericValue)) {
             const newData = [value[0], numericValue];
             setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
@@ -97,7 +138,8 @@ exports.RangeSlider = (0, react_1.forwardRef)((props, ref) => {
                             //doing .toString() to eliminate the leading zero bug
                             value: textFieldVal.lowerField.toString(), onChange: minChangeHandler, onBlur: textfieldBlurHandler, onKeyDown: textfieldKeyDownHandler, disabled: disabled, inputProps: { style: { textAlign: 'center' } } }) })), (0, jsx_runtime_1.jsx)(Box_1.Box, Object.assign({ pl: 4, pr: 4, display: 'flex', flex: 1 }, { children: (0, jsx_runtime_1.jsx)(Slider_1.Slider, { value: value, min: min, max: max, color: color ? color : 'primary', size: size, marks: marks, step: step, onChange: sliderChangeHandler, onBlur: (e) => onRangeBlur(e, value), onMouseUp: (e) => onSliderMouseUp(e, value), disabled: disabled, disableSwap: disableSwap }) })), (0, jsx_runtime_1.jsx)(Box_1.Box, Object.assign({ width: rightTextfieldWidth ? rightTextfieldWidth : 64, height: textfieldHeight ? textfieldHeight : 36 }, { children: (0, jsx_runtime_1.jsx)(NumberTextField, { label: "", 
                             //doing .toString() to eliminate the leading zero bug
-                            value: textFieldVal.upperField === max && showPlus ? `${textFieldVal.upperField}+` : textFieldVal.upperField.toString(), onChange: maxChangeHandler, onBlur: textfieldBlurHandler, onKeyDown: textfieldKeyDownHandler, disabled: disabled, inputProps: { style: { textAlign: 'center' } } }) }))] }))] })));
+                            // value={textFieldVal.upperField.toString()}
+                            value: textFieldVal.upperField === max && showPlus && plusToggle ? `${textFieldVal.upperField}+` : textFieldVal.upperField.toString(), onChange: maxChangeHandler, onBlur: textfieldBlurHandler, onKeyDown: textfieldKeyDownHandler, disabled: disabled, inputProps: { style: { textAlign: 'center' }, inputMode: 'numeric', pattern: '[0-9]*' } }) }))] }))] })));
 });
 exports.RangeSlider.defaultProps = {
     value: [0, 100],
--- dist/esm/components/RangeSlider/RangeSlider.js
+++ dist/esm/components/RangeSlider/RangeSlider.js
@@ -16,35 +16,55 @@ const NumberTextField = styled(TextField)(({ theme }) => ({
     '& .MuiInputBase-input': {
         padding: theme.spacing(2)
     }
-    // '& .MuiInputAdornment-positionEnd': {
-    //     marginRight: 0
-    // }
 }));
-function getAdjustedValues(valueArr, minVal, maxVal) {
-    let [value1, value2] = valueArr;
-    if (value1 > value2 || value1 > maxVal) {
-        // console.log('1', [value1, value2]);
-        value1 = value2 - 1;
-    }
-    if (value1 < minVal) {
-        // console.log('2', [value1, value2]);
-        value1 = minVal;
-    }
-    if (value2 < value1 || value2 < minVal) {
-        // console.log('3', [value1, value2]);
-        value2 = value1 + 1;
-    }
-    if (value2 > maxVal) {
-        // console.log('4', [value1, value2]);
-        value2 = maxVal;
-    }
-    return [value1, value2];
-}
 export const RangeSlider = forwardRef((props, ref) => {
     const { value, size, step, marks, min, max, color, headerTitle, disabled, rightTextfieldWidth, leftTextfieldWidth, textfieldHeight, onChange: onRangeChange, onRangeBlur = () => { }, onSliderMouseUp = () => { }, onTextfieldBlur = () => { }, onTextfieldEnterKeyDown = () => { }, showPlus, disableSwap } = props;
     const [textFieldVal, setTextFieldVal] = useState({ lowerField: value[0], upperField: value[1] });
+    const [activeThumbValue, setActiveThumbValue] = useState(textFieldVal.upperField);
+    const [plusToggle, setPlusToggle] = useState(true);
+    function getAdjustedValues(valueArr, minVal, maxVal) {
+        let [value1, value2] = valueArr;
+        if (value1 > value2 || value1 > maxVal) {
+            // console.log('1', [value1, value2]);
+            value1 = value2 - 1;
+        }
+        if (value1 < minVal) {
+            // console.log('2', [value1, value2]);
+            value1 = minVal;
+        }
+        if (value2 < value1 || value2 < minVal) {
+            // console.log('3', [value1, value2]);
+            value2 = value1 + 1;
+        }
+        if (value2 > maxVal) {
+            // console.log('4', [value1, value2]);
+            value2 = maxVal;
+        }
+        if (value1 === value2) {
+            if (activeThumbValue === 0) {
+                if (Math.min(value1, value2 - 1) < min) {
+                    value1 = min;
+                    value2 = value1 + 1;
+                }
+                else {
+                    value1 = Math.min(value1, value2 - 1);
+                }
+            }
+            else {
+                if (Math.max(value2, value1 + 1) > max) {
+                    value2 = max;
+                    value1 = value2 - 1;
+                }
+                else {
+                    value2 = Math.max(value2, value1 + 1);
+                }
+            }
+        }
+        return [value1, value2];
+    }
     useEffect(() => {
         setTextFieldVal(() => (Object.assign(Object.assign({}, textFieldVal), { lowerField: value[0], upperField: value[1] })));
+        setActiveThumbValue(textFieldVal.upperField);
     }, [value]);
     useEffect(() => {
         const adjustedValues = getAdjustedValues(value, min, max);
@@ -52,10 +72,25 @@ export const RangeSlider = forwardRef((props, ref) => {
             onRangeChange(adjustedValues);
         }
     }, [value[0], value[1]]);
-    const sliderChangeHandler = (e) => {
-        const data = e.target.value;
-        const newData = [Number(data[0]), Number(data[1])];
-        setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
+    useEffect(() => {
+        if (!plusToggle) {
+            setPlusToggle(true);
+        }
+    }, [textFieldVal.upperField]);
+    const sliderChangeHandler = (e, newValue, activeThumb) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+        let newData;
+        if (activeThumb === 0) {
+            newData = [Math.min(newValue[0], textFieldVal.upperField - 1), textFieldVal.upperField];
+            setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
+        }
+        else {
+            newData = [textFieldVal.lowerField, Math.max(newValue[1], textFieldVal.lowerField + 1)];
+            setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
+        }
+        setActiveThumbValue(() => activeThumb);
         onRangeChange(newData);
     };
     const minChangeHandler = (event) => {
@@ -67,8 +102,14 @@ export const RangeSlider = forwardRef((props, ref) => {
         }
     };
     const maxChangeHandler = (event) => {
-        const inputValue = event.target.value;
+        let inputValue = event.target.value;
+        if (inputValue.includes('+')) {
+            inputValue = inputValue.replace(/\+/g, '');
+        }
         const numericValue = Number(inputValue);
+        if (numericValue === max) {
+            return setPlusToggle(false);
+        }
         if (!isNaN(numericValue)) {
             const newData = [value[0], numericValue];
             setTextFieldVal(Object.assign(Object.assign({}, textFieldVal), { lowerField: newData[0], upperField: newData[1] }));
@@ -94,7 +135,8 @@ export const RangeSlider = forwardRef((props, ref) => {
                             //doing .toString() to eliminate the leading zero bug
                             value: textFieldVal.lowerField.toString(), onChange: minChangeHandler, onBlur: textfieldBlurHandler, onKeyDown: textfieldKeyDownHandler, disabled: disabled, inputProps: { style: { textAlign: 'center' } } }) })), _jsx(Box, Object.assign({ pl: 4, pr: 4, display: 'flex', flex: 1 }, { children: _jsx(Slider, { value: value, min: min, max: max, color: color ? color : 'primary', size: size, marks: marks, step: step, onChange: sliderChangeHandler, onBlur: (e) => onRangeBlur(e, value), onMouseUp: (e) => onSliderMouseUp(e, value), disabled: disabled, disableSwap: disableSwap }) })), _jsx(Box, Object.assign({ width: rightTextfieldWidth ? rightTextfieldWidth : 64, height: textfieldHeight ? textfieldHeight : 36 }, { children: _jsx(NumberTextField, { label: "", 
                             //doing .toString() to eliminate the leading zero bug
-                            value: textFieldVal.upperField === max && showPlus ? `${textFieldVal.upperField}+` : textFieldVal.upperField.toString(), onChange: maxChangeHandler, onBlur: textfieldBlurHandler, onKeyDown: textfieldKeyDownHandler, disabled: disabled, inputProps: { style: { textAlign: 'center' } } }) }))] }))] })));
+                            // value={textFieldVal.upperField.toString()}
+                            value: textFieldVal.upperField === max && showPlus && plusToggle ? `${textFieldVal.upperField}+` : textFieldVal.upperField.toString(), onChange: maxChangeHandler, onBlur: textfieldBlurHandler, onKeyDown: textfieldKeyDownHandler, disabled: disabled, inputProps: { style: { textAlign: 'center' }, inputMode: 'numeric', pattern: '[0-9]*' } }) }))] }))] })));
 });
 RangeSlider.defaultProps = {
     value: [0, 100],

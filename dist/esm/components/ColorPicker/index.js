6eef410d21d26a33ed31c4b8ef55677428c9c581
Abhishek Raj <abhishek.raj@algoscale.com>
2023-09-05 09:10:43 +0530

    created build

---
+++ dist/esm/components/ColorPicker/index.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { SketchPicker } from 'react-color';
+export const ColorPicker = (props) => {
+    return _jsx(SketchPicker, Object.assign({}, props));
+};
+//# sourceMappingURL=index.js.map

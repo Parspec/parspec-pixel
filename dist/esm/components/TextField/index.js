9026b42632e05221ccd3d9e68b987deed2c3c938
Gautam Kumar <gautam@parspec.io>
2023-09-27 11:51:26 +0530

    adding error class root.

--- dist/esm/components/TextField/index.js
+++ dist/esm/components/TextField/index.js
@@ -21,7 +21,7 @@ const StyledMUITextField = styled(MUITextField)({
     '& .MuiFormHelperText-root': {
         marginLeft: '0px'
     },
-    '& .Mui-error': {
+    '&.MuiFormHelperText-root Mui-error': {
         fontSize: '12px'
     }
 });

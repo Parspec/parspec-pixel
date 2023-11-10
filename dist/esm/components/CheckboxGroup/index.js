75721cc8563d78f8ea17cdddc0bf01075baa0682
Charul-Parspec <charul@parspec.io>
2023-11-10 18:02:28 +0530

    update checkbox group

--- dist/esm/components/CheckboxGroup/index.js
+++ dist/esm/components/CheckboxGroup/index.js
@@ -7,7 +7,7 @@ export const CheckboxGroup = function ({ label, size, options, onChange, error, 
             onChange(name, event.target.checked);
         };
     }
-    return (_jsxs(FormControl, Object.assign({ error: error }, { children: [_jsx(FormLabel, Object.assign({ component: "legend" }, { children: label })), _jsx(FormGroup, { children: options.map((checkboxInfo) => (_jsx(Checkbox, { checked: checkboxInfo.checked, disabled: disabled, onChange: handleCheckboxChange(checkboxInfo.name), name: checkboxInfo.name, label: checkboxInfo.label, size: size, color: color }))) }), helperText && _jsx(FormHelperText, { children: helperText })] })));
+    return (_jsxs(FormControl, Object.assign({ error: error }, { children: [_jsx(FormLabel, Object.assign({ component: "legend" }, { children: label })), _jsx(FormGroup, { children: options.map((checkboxInfo) => (_jsx(Checkbox, { checked: checkboxInfo.checked, disabled: disabled, onChange: handleCheckboxChange(checkboxInfo.name), name: checkboxInfo.name, label: checkboxInfo.label, size: size, color: color }))) }), helperText && _jsx(FormHelperText, Object.assign({ sx: { marginLeft: 0 } }, { children: helperText }))] })));
 };
 CheckboxGroup.defaultProps = {
     size: 'small',

c145e973f69d7c84e362b1b9aef7d68d917bb0ee
Gautam Kumar <gautam@parspec.io>
2023-11-27 10:15:58 +0530

    build

--- dist/cjs/components/Info/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { BoxProps } from '@mui/material';
-interface InfoProps extends Omit<BoxProps, 'border' | 'borderColor' | 'padding'> {
-}
-export declare function Info({ children, ...otherProps }: InfoProps): import("react/jsx-runtime").JSX.Element;
-export {};

c58b7711d01afab8aa96114c0e64017f42d93e09
kumarlovish <104341483+kumarlovish@users.noreply.github.com>
2024-01-03 18:38:43 +0530

    custom added for S3 file upload

--- src/components/FileSelector/SelectedFile.tsx
+++ src/components/FileSelector/SelectedFile.tsx
@@ -19,10 +19,11 @@ type SelectedFileProps = {
     index: number;
     handleResults: (data: {}, index: number) => void;
     isLoading?: boolean;
+    modifiedFileName?: boolean;
 };
 
 const SelectedFile = (props: SelectedFileProps) => {
-    const { file, onDelete, url, handleResults, index, isLoading } = props;
+    const { file, onDelete, url, handleResults, index, isLoading, modifiedFileName } = props;
     const [progress, setProgress] = useState(0);
     const [showProgress, setShowProgress] = useState(true);
 
@@ -35,7 +36,7 @@ const SelectedFile = (props: SelectedFileProps) => {
                 let response = await axios.post(
                     url,
                     {
-                        file_name: file.name
+                        file_name: modifiedFileName ? new Date().getTime() + file.name : file.name
                     },
                     {
                         headers: {
@@ -54,7 +55,8 @@ const SelectedFile = (props: SelectedFileProps) => {
                     cancelToken: source.token
                 });
                 setShowProgress(false);
-                return handleResults({ file, progress: 100 }, index);
+                let s3_file_path = response?.data?.s3_file_path;
+                return handleResults({ file, progress: 100, s3_file_path }, index);
             } catch (err: any) {
                 if (err?.message !== 'canceled') return handleResults({ file, error: err.message }, index);
             }

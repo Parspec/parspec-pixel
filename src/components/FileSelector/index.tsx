ca56a0c11b6001ab8de1dd1e45df9d1c11419ae0
kumarlovish <104341483+kumarlovish@users.noreply.github.com>
2024-01-04 13:04:11 +0530

    FileSelectorFileType changed

--- src/components/FileSelector/index.tsx
+++ src/components/FileSelector/index.tsx
@@ -16,6 +16,7 @@ export interface FileSelectorFileType {
     type?: string;
     webkitRelativePath?: string;
     filepath?: string;
+    s3_file_path?: string;
 }
 interface FileSelectorProps {
     maxFiles?: number;

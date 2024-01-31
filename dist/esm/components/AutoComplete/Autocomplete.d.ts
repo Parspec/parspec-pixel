911dd7f25fecf0cef4e154edb4d084a5714474c2
Atul midha <atulmidha@Atuls-MacBook-Pro.local>
2024-01-31 10:59:11 +0530

    build

--- dist/esm/components/AutoComplete/Autocomplete.d.ts
+++ dist/esm/components/AutoComplete/Autocomplete.d.ts
@@ -32,6 +32,7 @@ export interface AutocompleteProps {
     open?: boolean;
     maxLength?: number;
     sx?: SxProps;
+    inputProps?: any;
     loading?: boolean;
 }
 export declare const Autocomplete: React.FC<AutocompleteProps>;
--- dist/esm/components/AutoComplete/Autocomplete.js
+++ dist/esm/components/AutoComplete/Autocomplete.js
@@ -1,74 +1,27 @@
-var __rest =
-    (this && this.__rest) ||
-    function (s, e) {
-        var t = {};
-        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
-        if (s != null && typeof Object.getOwnPropertySymbols === 'function')
-            for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-                if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
-            }
-        return t;
-    };
-import { jsx as _jsx, Fragment as _Fragment } from 'react/jsx-runtime';
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
 import { forwardRef, useState, useEffect } from 'react';
 import { TextField } from '../TextField';
 import { default as MUIAutocomplete, createFilterOptions } from '@mui/material/Autocomplete';
 const filter = createFilterOptions();
 export const Autocomplete = forwardRef((_a, ref) => {
-    var {
-            id,
-            label,
-            placeholder,
-            color,
-            variant,
-            onChange,
-            optionlabelkeyname,
-            freeSolo,
-            fieldSize,
-            onBlur = () => {},
-            helperText,
-            error,
-            options,
-            onTextFieldChange,
-            limitTags,
-            disabled,
-            value,
-            autoFocus,
-            blurOnEmptyInput,
-            maxLength = 255,
-            filterOptionsCallBack = (options, params) => {
-                let filteredOptions = filter(options, params);
-                if (typeof state === 'object' && state[optionlabelkeyname]) {
-                    filteredOptions = options.filter((option) => option[optionlabelkeyname] === state[optionlabelkeyname]);
-                }
-                return filteredOptions;
-            },
-            sx,
-            loading
-        } = _a,
-        props = __rest(_a, [
-            'id',
-            'label',
-            'placeholder',
-            'color',
-            'variant',
-            'onChange',
-            'optionlabelkeyname',
-            'freeSolo',
-            'fieldSize',
-            'onBlur',
-            'helperText',
-            'error',
-            'options',
-            'onTextFieldChange',
-            'limitTags',
-            'disabled',
-            'value',
-            'maxLength',
-            'filterOptionsCallBack',
-            'sx',
-            'loading'
-        ]);
+    var { id, label, placeholder, color, variant, onChange, optionlabelkeyname, freeSolo, fieldSize, onBlur = () => { }, helperText, error, options, onTextFieldChange, limitTags, disabled, value, autoFocus, blurOnEmptyInput, maxLength = 255, filterOptionsCallBack = (options, params) => {
+        let filteredOptions = filter(options, params);
+        if (typeof state === 'object' && state[optionlabelkeyname]) {
+            filteredOptions = options.filter((option) => option[optionlabelkeyname] === state[optionlabelkeyname]);
+        }
+        return filteredOptions;
+    }, sx, inputProps, loading } = _a, props = __rest(_a, ["id", "label", "placeholder", "color", "variant", "onChange", "optionlabelkeyname", "freeSolo", "fieldSize", "onBlur", "helperText", "error", "options", "onTextFieldChange", "limitTags", "disabled", "value", "autoFocus", "blurOnEmptyInput", "maxLength", "filterOptionsCallBack", "sx", "inputProps", "loading"]);
     const [state, setState] = useState(value || '');
     const handleOnChange = (event, newValue) => {
         onChange(Object.assign(Object.assign({}, event), { target: Object.assign(Object.assign({}, event.target), { value: newValue }) }));
@@ -94,8 +47,10 @@ export const Autocomplete = forwardRef((_a, ref) => {
             }
             setState(inputValue);
             onBlur(inputValue);
-        } else {
-            if (blurOnEmptyInput) blurOnEmptyInput(inputValue);
+        }
+        else {
+            if (blurOnEmptyInput)
+                blurOnEmptyInput(inputValue);
         }
     };
     const handleOnInputChange = (event, value) => {
@@ -104,46 +59,15 @@ export const Autocomplete = forwardRef((_a, ref) => {
             onTextFieldChange(event, value);
         }
     };
-    return _jsx(_Fragment, {
-        children: _jsx(
-            MUIAutocomplete,
-            Object.assign({ fullWidth: true }, props, {
-                options: options,
-                ref: ref,
-                sx: sx,
-                id: id,
-                onBlur: handleFocusOut,
-                onChange: handleOnChange,
-                getOptionLabel: (option) => {
-                    if (typeof option === 'object') {
-                        return `${option[optionlabelkeyname]}`;
-                    }
-                    return option;
-                },
-                value: value,
-                limitTags: limitTags,
-                filterOptions: filterOptions,
-                onInputChange: handleOnInputChange,
-                freeSolo: freeSolo,
-                renderInput: (_a) => {
-                    var { size } = _a,
-                        params = __rest(_a, ['size']);
-                    return _jsx(
-                        TextField,
-                        Object.assign({ size: fieldSize, helperText: helperText, error: error }, params, {
-                            variant: variant,
-                            color: color,
-                            label: label,
-                            placeholder: placeholder,
-                            inputProps: Object.assign(Object.assign({}, params.inputProps), { maxLength })
-                        })
-                    );
-                },
-                disabled: disabled,
-                loading: loading
-            })
-        )
-    });
+    return (_jsx(_Fragment, { children: _jsx(MUIAutocomplete, Object.assign({ fullWidth: true }, props, { options: options, ref: ref, sx: sx, id: id, onBlur: handleFocusOut, onChange: handleOnChange, getOptionLabel: (option) => {
+                if (typeof option === 'object') {
+                    return `${option[optionlabelkeyname]}`;
+                }
+                return option;
+            }, value: value, limitTags: limitTags, filterOptions: filterOptions, onInputChange: handleOnInputChange, freeSolo: freeSolo, renderInput: (_a) => {
+                var { size } = _a, params = __rest(_a, ["size"]);
+                return (_jsx(TextField, Object.assign({ size: fieldSize, helperText: helperText, error: error }, params, { variant: variant, color: color, label: label, placeholder: placeholder, autoFocus: autoFocus, inputProps: Object.assign(Object.assign(Object.assign({}, params.inputProps), inputProps), { maxLength }) })));
+            }, disabled: disabled, loading: loading })) }));
 });
 Autocomplete.defaultProps = {
     color: 'primary',

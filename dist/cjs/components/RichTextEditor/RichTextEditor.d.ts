37bee87d8bd59947c7e512dfd85f62c7886b8599
Sunny Pathak <sunnypathak@Sunnys-MacBook-Pro.local>
2024-03-21 12:58:50 +0530

    remove unwanted file

--- dist/cjs/components/RichTextEditor/RichTextEditor.d.ts
+++
@@ -1,3 +0,0 @@
-import './RichText.css';
-import { IRichTextEditorProps } from './types';
-export declare const RichTextEditor: ({ onFileUpload, initialHtml, editorBgColor, contentEditableHeight, isDisable, placeHolderText, showAttachements, showShareableLinkButton, shareableLinkUrl, shareableLinkTitle, showFontFamiliy, onBlur, onChange, onFocus }: IRichTextEditorProps) => import("react/jsx-runtime").JSX.Element;
